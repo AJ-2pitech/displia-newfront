5162f98db9c2675a76c2e2ea0c32a28b1e51cd92
DijaaVoo <dida@dodo.com>
2023-02-28 23:55:50 +0530

    f*ck f*ck

--- src/config/api.js
+++ src/config/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API_URL = 'http://localhost:8000/api/v1/';
-const PRODUCTION_API = "https://displaylab-back-final-production.up.railway.app"
+const PRODUCTION_API = "https://displaylab-back-final-production.up.railway.app/api/v1/"
 
 const axiosInstance = axios.create({
     baseURL: PRODUCTION_API,
