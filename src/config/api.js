1c14c60bb4b371127f9c9647523a7db559331d0a
DijaaVoo <dida@dodo.com>
2023-02-28 23:46:43 +0530

    Added public server url

--- src/config/api.js
+++ src/config/api.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 const API_URL = 'http://localhost:8000/api/v1/';
-const PRODUCTION_API = "http://localhost:8000/api/v1/"
+const PRODUCTION_API = "https://displaylab-back-final-production.up.railway.app"
 
 const axiosInstance = axios.create({
-    baseURL: API_URL,
+    baseURL: PRODUCTION_API,
     withCredentials: true
 });
 
